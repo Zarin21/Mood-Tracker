41ae085e41c2fa0f260adcde32cc99957b827d85
Levi <leviliu@Levis-MacBook-Pro.local>
2025-03-07 00:43:52 -0700

    implemented sub issue 1.1.1.3

--- code/app/src/main/java/com/example/unemployedavengers/Dashboard.java
+++ code/app/src/main/java/com/example/unemployedavengers/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.unemployedavengers;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.Navigation;
 
 import com.example.unemployedavengers.databinding.DashboardBinding;
+import com.example.unemployedavengers.models.MoodEvent;
 
 public class Dashboard extends Fragment {
     private DashboardBinding binding;
@@ -41,6 +44,19 @@ public class Dashboard extends Fragment {
                     .navigate(R.id.action_dashboardFragment_to_inputDialog);
 
 
+        });
+
+
+        getParentFragmentManager().setFragmentResultListener("input_dialog_result", getViewLifecycleOwner(), new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                // Retrieve the MoodEvent object
+                MoodEvent moodEvent = (MoodEvent) result.getSerializable("mood_event_key");
+                if (moodEvent != null) {
+                    Log.d("Dashboard", "Mood event received");
+                    //addMoodEvent(moodEvent); // Store the mood event in Firestore
+                }
+            }
         });
 
     }
