677f5bc0995d59b4db4d717f4546364afab04a7b
Levi <leviliu@Levis-MacBook-Pro.local>
2025-03-08 19:57:37 -0700

    implmented edit in history

--- code/app/src/main/java/com/example/unemployedavengers/History.java
+++ code/app/src/main/java/com/example/unemployedavengers/History.java
@@ -7,15 +7,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.Navigation;
 
 import com.example.unemployedavengers.arrayadapters.MoodEventArrayAdapter;
 import com.example.unemployedavengers.databinding.HistoryBinding;
 import com.example.unemployedavengers.models.MoodEvent;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -57,7 +61,45 @@ public class History extends Fragment {
         binding.historyList.setAdapter(moodAdapter);
 
         loadHistoryMoodEvents(); //load all mood events
+
+        //register the listener for the result from InputDialog (Only once)
+        getParentFragmentManager().setFragmentResultListener("input_dialog_result", getViewLifecycleOwner(), new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                //retrieve the MoodEvent object
+                MoodEvent moodEvent = (MoodEvent) result.getSerializable("mood_event_key");
+
+                //existed is true if we are updating a mood, else we are adding a mood
+                if (moodEvent.getExisted()==true) {
+                    updateMoodEvent(moodEvent);
+                }
+            }
+        });
+
     }
+    private void updateMoodEvent(MoodEvent moodEvent) {
+        //NEED TO RELOAD DATABASE CAUSE FIREBASE IS AN IDIOT (crashes if you add a moodEvent and tries to update it right away cause "cannot find id")
+        loadHistoryMoodEvents();
+
+        String moodEventId = moodEvent.getId();
+
+        Log.d("Dashboard", "updateMoodEvent: " + moodEventId);
+        //get document via id
+        DocumentReference moodEventDocRef = moodEventRef.document(moodEventId);
+
+        //set the new values for the document
+        moodEventDocRef.set(moodEvent)  //use set() to update or create the document if it doesn't exist
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(getContext(), "Mood updated successfully", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), "Failed to update mood", Toast.LENGTH_SHORT).show();
+                });
+
+
+
+    }
+
     private void loadHistoryMoodEvents() {
         moodEventRef.get()
                 .addOnCompleteListener(task -> {
