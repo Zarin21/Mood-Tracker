2d2935d1e2956c31418a675cc3aa96e8300819ae
Levi <leviliu@Levis-MacBook-Pro.local>
2025-03-14 18:26:24 -0600

    updated mood event constructor

--- code/app/src/main/java/com/example/unemployedavengers/models/MoodEvent.java
+++ code/app/src/main/java/com/example/unemployedavengers/models/MoodEvent.java
@@ -30,7 +30,7 @@ public class MoodEvent implements Serializable {
     private boolean publicStatus;
 
     //Constructor
-    public MoodEvent(String mood, String reason, String trigger, String situation, long time, String radioSituation, String imageUri, Boolean publicStatus) {
+    public MoodEvent(String mood, String reason, String trigger, String situation, long time, String radioSituation, String imageUri, boolean publicStatus) {
         this.mood = mood;
         this.reason = reason;
         this.trigger = trigger;
