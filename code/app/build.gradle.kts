00b91c7c1329c4afa786d110fc220d895263e37c
Ethan Shi <ethanshi322@gmail.com>
2025-03-18 01:34:27 -0600

    done for this user story

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     implementation(libs.firebase.storage)
     implementation("com.google.firebase:firebase-storage:20.2.1")
 
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+
     // Test Dependencies
     testImplementation(libs.junit)
     testImplementation("org.mockito:mockito-core:4.11.0")
